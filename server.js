a15fc048a7445466b89f9c846533c3ab66888be0
RaafetDev <121636624+RaafetDev@users.noreply.github.com>
2024-05-28 02:29:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const Jimp = require('jimp');
 const Tesseract = require('tesseract.js');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
